dadc6d8f1de60fdbd63d43a7af4642a74206060a
AmirKasra_MomenKashani <71598934+AmirKasraKashani@users.noreply.github.com>
2022-05-24 21:21:08 +0430

    Update CarRental.lua

--- CarRental.lua
+++ CarRental.lua
@@ -30,7 +30,7 @@ Citizen.CreateThread(function()
 	while QBCore == nil do
 		TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 		Citizen.Wait(0) -- Saniye Bekletme
-	   end
+	end
 	
 	WarMenu.CreateMenu('carRental', 'Joker City Car Rental')
 	WarMenu.CreateSubMenu('closeMenu', 'carRental', 'Are you sure?')
